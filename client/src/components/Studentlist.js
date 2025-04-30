d06e5d2442c00fddf2c81412a1efc43d688581f9
nishanth1646 <nishanthreddy1646@gmail.com>
2025-04-30 14:43:27 +0530

    Update Studentlist.js

--- client/src/components/Studentlist.js
+++ client/src/components/Studentlist.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Studentlist() {
-    const [studentList, setStudentList] = useState([]);
+    const [studentList, setStudentList] = useState([])
     const [isLoading, setIsLoading] = useState(true);
     const [searchTerm, setSearchTerm] = useState("");
 
